6e4a157160ec45668515fe2cfbf3fe86a0d3a6b9
Christen Lofland <christen@lofland.net>
2024-10-28 16:01:06 -0500

    Clean up Photon container a little bit.

--- dawarich/photon/entrypoint.sh
+++ dawarich/photon/entrypoint.sh
@@ -4,8 +4,9 @@
 if [ ! -d "/photon/photon_data/elasticsearch" ]; then
     echo "Downloading search index"
 
-    # Let graphhopper know where the traffic is coming from
-    USER_AGENT="docker: tonsnoei/photon-geocoder"
+    # Information about this download can be found here:
+    # https://nominatim.org/2020/10/21/photon-country-extracts.html
+    # The data is updated weekly, although I doubt much changes in the geocoding data for the areas I typically travel.
     # If you want to install a specific region only, enable the line below and disable the current 'wget' row.
     # Take a look at http://download1.graphhopper.com/public/extracts/by-country-code for your country
     # wget --user-agent="$USER_AGENT" -O - http://download1.graphhopper.com/public/extracts/by-country-code/nl/photon-db-nl-latest.tar.bz2 | bzip2 -cd | tar x
