74a2f2b8a3b4eb4a829a84bc72ce055d8ce2c6b5
Christen Lofland <christen@lofland.net>
2025-05-29 11:57:54 -0500

    Healthcheck for ghost

---
+++ caddy/healthcheck/get-ghost-page.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"crypto/tls"
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	// Create a custom HTTP client that ignores certificate validation
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tr}
+
+	resp, err := client.Get("http://localhost:2368/ghost/api/admin/site/")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("Response status:", resp.Status)
+
+	scanner := bufio.NewScanner(resp.Body)
+	for i := 0; scanner.Scan() && i < 5; i++ {
+		fmt.Println(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+}
